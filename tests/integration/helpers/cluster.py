3c29685f315a49e9736f13033791ebfcead936bb
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2025-10-12 13:57:28 +0200

    Integration tests: remove seq tests

--- tests/integration/helpers/cluster.py
+++ tests/integration/helpers/cluster.py
@@ -4219,6 +4219,8 @@ class ClickHouseInstance:
             self.mem_limit = "mem_limit : 10g"
         if cpu_limit is not None:
             self.cpu_limit = f"cpus : {cpu_limit}"
+        elif cpu_limit is False:
+            self.cpu_limit = ""
         else:
             self.cpu_limit = "cpus : 6"
 
